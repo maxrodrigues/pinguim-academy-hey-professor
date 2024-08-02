800e21358464ae8a3e6620c27c589f364dcdd68e
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-08-02 16:58:55 -0300

    testing pre-commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,11 +12,8 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
-    {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
+    public function edit(Request $request): View{
+        return view('profile.edit', ['user' => $request->user(),]);
     }
 
     /**
