d9b623d6eac5aa3a76e1116cc3e56c395e335409
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-08-06 15:12:11 -0300

    HEY-4: it should habe at least 10 characters when creating a new question

--- tests/Feature/CreateQuestionTest.php
+++ tests/Feature/CreateQuestionTest.php
@@ -16,3 +16,15 @@ it('should be able to create a new question bigger than 255 characters', functio
     assertDatabaseCount('questions', 1);
     assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 });
+
+it('should have at least 10 characters', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = $this->post(route('questions.store'), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+});
