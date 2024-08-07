f71b74d9517d83199121bee2efda0974a0939a32
Max Rodrigues <maxuel.rodrigues@gmail.com>
2024-08-07 09:14:30 -0300

    HEY-3: fixing phpstan errors

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Database\Factories\QuestionFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    /** @use HasFactory<QuestionFactory> */
     use HasFactory;
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,6 +11,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
     use Notifiable;
 
